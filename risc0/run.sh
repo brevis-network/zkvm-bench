6a5265f721169cd5bd9575baefe2e0c472a8d596
kaiwei-0 <kaiwei@brevis.network>
2025-02-21 18:09:17 +0800

    Update script

--- risc0/run.sh
+++ risc0/run.sh
@@ -8,10 +8,10 @@ if [ ! -d "../../logs" ]; then
 fi
 
 commands=(
-  ./eval.sh fibonacci300kn risc0 poseidon 21 benchmark > ../../logs/risc0_f_300kn.log
-  ./eval.sh tendermint risc0 poseidon 21 benchmark > ../../logs/risc0_tendermint.log
-  ./eval.sh rsp20528709 risc0 poseidon 21 benchmark > ../../logs/risc0_rsp20528709.log
-  ./eval.sh rsp17106222 risc0 poseidon 21 benchmark > ../../logs/risc0_rsp17106222.log
+  "./eval.sh fibonacci300kn risc0 poseidon 21 benchmark > ../../logs/risc0_f_300kn.log"
+  "./eval.sh tendermint risc0 poseidon 21 benchmark > ../../logs/risc0_tendermint.log"
+  "./eval.sh rsp20528709 risc0 poseidon 21 benchmark > ../../logs/risc0_rsp20528709.log"
+  "./eval.sh rsp17106222 risc0 poseidon 21 benchmark > ../../logs/risc0_rsp17106222.log"
 )
 
 for cmd in "${commands[@]}"; do
