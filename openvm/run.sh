cb1845744a9bd08036c6141caa27bd74fbb5be9c
kaiwei-0 <kaiwei@brevis.network>
2025-02-21 23:15:13 +0800

    use one single logs dir

--- openvm/run.sh
+++ openvm/run.sh
@@ -2,21 +2,21 @@
 set -ex
 
 source .env
-mkdir -p openvm_logs
+mkdir -p ../logs
 
 pushd openvm-benchmark
 
 echo "Benchmarking fibonacci"
-RPC_1=$RPC_1 APP=fibonacci bash run.sh >../openvm_logs/fib_300k.log
+RPC_1=$RPC_1 APP=fibonacci bash run.sh >../../logs/openvm-fib_300k.log
 
 echo "Benchmarking tendermint"
-RPC_1=$RPC_1 APP=tendermint bash run.sh >../openvm_logs/tendermint.log
+RPC_1=$RPC_1 APP=tendermint bash run.sh >../../logs/openvm-tendermint.log
 
 echo "Benchmarking reth-17106222"
-RPC_1=$RPC_1 APP=reth BLOCK_NUMBER=17106222 bash run.sh >../openvm_logs/reth_17106222.log
+RPC_1=$RPC_1 APP=reth BLOCK_NUMBER=17106222 bash run.sh >../../logs/openvm-reth_17106222.log
 
 echo "Benchmarking reth-20528709"
-RPC_1=$RPC_1 APP=reth BLOCK_NUMBER=20528709 bash run.sh >../openvm_logs/reth_20528709.log
+RPC_1=$RPC_1 APP=reth BLOCK_NUMBER=20528709 bash run.sh >../../logs/openvm-reth_20528709.log
 
 popd
 echo "openvm benchmark complete!"
